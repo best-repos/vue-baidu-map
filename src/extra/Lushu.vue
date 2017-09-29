8fcd36684c8a85c8bbcf50b7992457f82b4b9d83
Alex Chen <nwwind@outlook.com>
2017-09-29 15:16:47 +0800

    add step to  lushu

--- src/extra/Lushu.vue
+++ src/extra/Lushu.vue
@@ -1,12 +1,12 @@
 <script>
 /*eslint-disable*/
 import commonMixin from '@/base/mixins/common.js'
-import {createIcon} from '@/base/factory.js'
+import { createIcon } from '@/base/factory.js'
 import Lushu from 'bmaplib.lushu'
 
 export default {
   name: 'bm-lushu',
-  render (h) {},
+  render(h) { },
   mixins: [commonMixin('lushu')],
   props: {
     path: {
@@ -15,7 +15,7 @@ export default {
     },
     landmarkPois: {
       type: Array,
-      default () {
+      default() {
         return []
       }
     },
@@ -49,56 +49,56 @@ export default {
   },
   watch: {
     path: {
-      handler (val) {
+      handler(val) {
         this.reload()
       },
       deep: true
     },
     landmarkPois: {
-      handler (val) {
+      handler(val) {
         this.reload()
       },
       deep: true
     },
     icon: {
-      handler (val) {
-        const {originInstance, content} = this
+      handler(val) {
+        const { originInstance, content } = this
         const newMarker = createIcon(BMap, val)
         originInstance._opts.icon = newMarker
         originInstance._marker = newMarker
       },
       deep: true
     },
-    speed (val) {
-      const {originInstance, content} = this
+    speed(val) {
+      const { originInstance, content } = this
       originInstance._opts.speed = val
     },
-    content (val) {
-      const {originInstance, infoWindow} = this
+    content(val) {
+      const { originInstance, infoWindow } = this
       val && infoWindow ? originInstance.showInfoWindow() : originInstance.hideInfoWindow()
       originInstance._opts.defaultContent = val
       originInstance._overlay && originInstance._overlay.setHtml(val)
     },
-    autoView (val) {
-      const {originInstance, content} = this
+    autoView(val) {
+      const { originInstance, content } = this
       originInstance._opts.autoView = val
     },
-    rotation (val) {
-      const {originInstance, content} = this
+    rotation(val) {
+      const { originInstance, content } = this
       originInstance._opts.enableRotation = val
     },
-    infoWindow (val) {
-      const {originInstance, content} = this
+    infoWindow(val) {
+      const { originInstance, content } = this
       originInstance && val && content ? originInstance.showInfoWindow() : originInstance.hideInfoWindow()
     },
-    play (val) {
-      const {originInstance} = this
+    play(val) {
+      const { originInstance } = this
       val && originInstance ? originInstance.start() : !this._isEnd && originInstance.pause()
     }
   },
   methods: {
-    load () {
-      const {BMap, map, path, landmarkPois, icon, speed, content, autoView, rotation, infoWindow, play} = this
+    load() {
+      const { BMap, map, path, landmarkPois, icon, speed, content, autoView, rotation, infoWindow, play } = this
       const lushu = this.originInstance = new Lushu(map, path, {
         enableRotation: rotation,
         landmarkPois,
@@ -113,7 +113,10 @@ export default {
         },
         onstop: e => {
           this._isEnd = true
-          this.$emit('stop')
+          this.$emit('stop', e)
+        },
+        onstep: e => {
+          this.$emit('step', {});
         },
         onpause: e => this.$emit('pause')
       })
